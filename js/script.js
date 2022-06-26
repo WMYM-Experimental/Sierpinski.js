b82dc097eeb59a6473e04bbe3fc4fafa346784d4
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-25 21:14:51 -0500

    change the idea of the implementation, add a point class

--- js/script.js
+++ js/script.js
@@ -4,48 +4,36 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const config = {
-    x: canvas.width / 2 + 200 / 2,
-    y: canvas.height / 2 + 200 / 2,
-    size: 200,
-};
+let size = 200;
 
-const getRandomNumebr = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.random() * (max - min) + min;
-};
-
-const drawTriangle = (x, y, size) => {
-    if (size < 10) {
-        return;
-    } else {
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.beginPath();
-        ctx.arc(x, y, 2, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.lineTo(x - size, y);
-        ctx.lineTo(
-            x - size / 2,
-            y - Math.sqrt(Math.pow(size, 2) - Math.pow(size / 2, 2))
-        );
-        ctx.closePath();
-        ctx.stroke();
-        drawTriangle(x - size / 2, y, size * 0.8);
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, size / 2, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+const p0 = new Point(canvas.width / 2 + size / 2, canvas.height / 2 + size / 2);
+const p1 = new Point(p0.x - size, p0.y);
+const p2 = new Point(
+    p0.x - size / 2,
+    p0.y - Math.sqrt(Math.pow(size, 2) - Math.pow(size / 2, 2))
+);
+
+const drawTriangle = (pA, pB, pC) => {
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.moveTo(pA.x, pA.y);
+    ctx.stroke();
+    ctx.lineTo(pB.x, pB.y);
+    ctx.lineTo(pC.x, pC.y);
+    ctx.closePath();
+    ctx.stroke();
 };
 
-const drawCircle = (x, y, size) => {
-    if (size < 10) {
-        return;
-    } else {
-        ctx.strokeStyle = "red";
-        ctx.beginPath();
-        ctx.arc(x, y, size, 0, 2 * Math.PI);
-        ctx.stroke();
-        drawCircle(x, y, size / 2);
-    }
-};
-drawTriangle(config.x, config.y, config.size);
+drawTriangle(p0, p1, p2);

ae01b7498fdf866cf0a390558688bf468105ed9b
WashingtonYandun <washington.yandun06@gmail.com>
2022-06-25 21:53:14 -0500

    finish the porject

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let size = 200;
+let size = Math.ceil(canvas.width / 2.5);
 
 class Point {
     constructor(x, y) {
@@ -17,7 +17,6 @@ class Point {
         ctx.fill();
     }
 }
-
 const p0 = new Point(canvas.width / 2 + size / 2, canvas.height / 2 + size / 2);
 const p1 = new Point(p0.x - size, p0.y);
 const p2 = new Point(
@@ -26,14 +25,29 @@ const p2 = new Point(
 );
 
 const drawTriangle = (pA, pB, pC) => {
-    ctx.strokeStyle = "red";
+    ctx.strokeStyle = "#006d77";
     ctx.beginPath();
     ctx.moveTo(pA.x, pA.y);
-    ctx.stroke();
     ctx.lineTo(pB.x, pB.y);
     ctx.lineTo(pC.x, pC.y);
     ctx.closePath();
     ctx.stroke();
 };
 
-drawTriangle(p0, p1, p2);
+const sierpinski = (p0, p1, p2) => {
+    drawTriangle(p0, p1, p2);
+
+    let pA = new Point((p0.x + p1.x) / 2, (p0.y + p1.y) / 2);
+    let pB = new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+    let pC = new Point((p2.x + p0.x) / 2, (p2.y + p0.y) / 2);
+
+    let size = p0.x - p1.x;
+
+    if (size > 1) {
+        sierpinski(p0, pA, pC);
+        sierpinski(pA, p1, pB);
+        sierpinski(pC, pB, p2);
+    }
+};
+
+sierpinski(p0, p1, p2);
